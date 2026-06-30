5d3487b91b4e53b6c6bb1b2023ca14213f57e9f9
Driftocity <69994669+Driftocity@users.noreply.github.com>
2026-06-30 11:15:59 -0400

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Thanks! We will contact you shortly for your estimate.");
-    e.target.reset();
-  });
-});
