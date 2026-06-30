01016e788d2b39e816a4a1e79ec7c662bc898f24
Driftocity <69994669+Driftocity@users.noreply.github.com>
2026-06-30 10:01:07 -0400

    Fix formatting and remove unnecessary newlines

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   document.querySelector(".form").addEventListener("submit", (e) => {
     e.preventDefault();
     alert("Thanks! We will contact you shortly for your estimate.");
     e.target.reset();
   });
-
 });
